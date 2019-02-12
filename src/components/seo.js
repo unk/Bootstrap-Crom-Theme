e2b01135a4a0ab76dbd743e3b89e5dfbd21a3588
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2019-02-12 15:33:51 +0900

    og:image 추가

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql, withPrefix } from 'gatsby';
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
@@ -33,6 +33,10 @@ function SEO({ description, lang, meta, keywords, title }) {
               {
                 property: `og:type`,
                 content: `website`
+              },
+              {
+                property: `og:image`,
+                content: `https://bootstrap-crom-theme.unk.io/${withPrefix('images/og-image.png')}`
               },
               {
                 name: `twitter:card`,
